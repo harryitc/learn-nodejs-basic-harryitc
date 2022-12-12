86d85d5016edd74e65c010cb9bf6c5a1ee288a76
Phan Ngọc Cường <cuongharryit@gmail.com>
2022-12-12 21:35:04 +0700

    video5

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-  console.log('run request ...')
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<h3>Hello World!</h3>')
-  res.write('<h2>from Harryitc </h2>')
-  res.end()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! with harryitc')
 })
 
-server.listen(3000, 'localhost', () => {
-  console.log('Node.JS server is running on port: 3000')
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
