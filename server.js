18ac754c05ea521962d90b16a01494b57fff91ef
Phan Ngọc Cường <cuongharryit@gmail.com>
2022-12-12 22:46:47 +0700

    json use type module va day la code khi tu tim hieu

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express')
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 const app = express()
 const port = 3000
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 app.get('/', (req, res) => {
-  res.send('Hello World! with harryitc')
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.listen(port, () => {
