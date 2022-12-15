dfec1a4bfa2429bb89b22c27acda0b5c635db923
Phan Ngọc Cường <cuongharryit@gmail.com>
2022-12-15 18:40:25 +0700

    video9 HTTP REQUEST METHOD & SỬ DỤNG DATABASE VỚI NODE.JS

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,17 @@
+import connection from '../configs/connectDB'
+
 let getHompage = (req, res) => {
   // logic
-  return res.render('test/index.ejs')
+  // simple query
+  let data = []
+  connection.query('SELECT * FROM `users`', function (err, results, fields) {
+    console.log(results) // results contains rows returned by server
+    data = results.map((row) => {
+      return row
+    })
+    return res.render('test/index.ejs', { dataUser: JSON.stringify(data) })
+  })
+  // console.log('>>> check data: ', typeof data, JSON.stringify(data))
 }
 module.exports = {
   getHompage,
