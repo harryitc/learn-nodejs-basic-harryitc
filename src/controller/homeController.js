49e981d94c9ebf3a95d03096cdc7bea95ad02874
Phan Ngọc Cường <cuongharryit@gmail.com>
2022-12-15 23:46:41 +0700

    video11 form -  ROUTER PARAMS - Xây dựng tra chi tiết users - js destructuring - async await - pool.excute

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,23 +1,19 @@
-import connection from '../configs/connectDB'
+import { json } from 'body-parser'
+import pool from '../configs/connectDB'
 
-let getHompage = (req, res) => {
-  // logic
-  // simple query
-  let data = []
-  connection.query('SELECT * FROM `users`', function (err, results, fields) {
-    console.log(results) // results contains rows returned by server
-    data = results.map((row) => {
-      return row
-    })
-
-    // lệnh render này sẽ trả về view. Và gồm 2 tham số bên dưới.
-    // Tham số 1: target (file views mà bạn muốn truyền vào)
-    // tham số 2: key : value , bên dưới là key dataUser là 1 biến. và data: là 1 giá trị
-    // Khi dùng key, muốn in ra biến dataUser từ file views là: <%= dataUser %>
-    return res.render('index.ejs', { dataUser: data })
-  })
-  // console.log('>>> check data: ', typeof data, JSON.stringify(data))
+let getHompage = async (req, res) => {
+  const [rows, fields] = await pool.execute('SELECT * FROM users')
+  return res.render('index.ejs', { dataUser: rows })
 }
+let getDetailPage = async (req, res) => {
+  let userId = req.params.id
+  let user = await pool.execute('select * from users where id = ?', [userId])
+
+  console.log('check req params: ', user[0])
+  return res.send(JSON.stringify(user[0]))
+}
+
 module.exports = {
   getHompage,
+  getDetailPage,
 }
